8bddfa4f1a16cdfeab136209031f18efb48062c1
mihailalekssev <mihailaleksseev@yandex.ru>
2018-07-31 00:23:28 +0300

    some new functional

--- src/DrumPad.java
+++ src/DrumPad.java
@@ -1,33 +1,63 @@
-import javax.sound.midi.*;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import javax.sound.midi.*;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.net.Socket;
 import java.util.*;
 
 public class DrumPad {
 
+    JFrame theFrame;
     JPanel mainPanel;
+    JList incomingList;
     ArrayList<JCheckBox> checkboxList;
+    int nextNum;
+    Vector<String> lineVector = new Vector<String>();
+    String userName;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    HashMap<String, boolean[]> otherSeqsMap = new HashMap<String, boolean[]>();
+
     Sequencer sequencer;
     Sequence sequence;
+    Sequence mySequence = null;
     Track track;
-    JFrame theFrame;
+
 
     String[] insrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga",
     "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"};
     int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
+    public void startUp(String name) {
+        userName = name;
+        try {
+            Socket sock = new Socket("127.0.0.1",5252);
+            out = new ObjectOutputStream(sock.getOutputStream());
+            in = new ObjectInputStream(sock.getInputStream());
+            Thread remote = new Thread(new RemoteReader());
+            remote.start();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            System.out.println("StartUp");
+        }
+        setUpMidi();
+        buildGUI();
+    }
+
     public void buildGUI() {
         theFrame = new JFrame("Drum Pad");
-        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10 ,10));
 
         checkboxList = new ArrayList<JCheckBox>();
-        Box buttonBox = new Box(BoxLayout.Y_AXIS);
 
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
         JButton start = new JButton("Start");
         start.addActionListener(new MyStartListener());
         buttonBox.add(start);
@@ -44,8 +74,22 @@ public class DrumPad {
         downTempo.addActionListener(new MyDownTempoListener());
         buttonBox.add(downTempo);
 
+        JButton sendIt = new JButton("Send it");
+        sendIt.addActionListener(new MySendListener());
+        buttonBox.add(sendIt);
+
+        JTextField userMessage = new JTextField();
+        buttonBox.add(userMessage);
+
+        incomingList = new JList();
+        incomingList.addListSelectionListener(new MyListSelectionListener());
+        incomingList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane theList = new JScrollPane(incomingList);
+        buttonBox.add(theList);
+        incomingList.setListData(lineVector);
+
         Box nameBox = new Box(BoxLayout.Y_AXIS);
-        for (int i = 0; i < 16; i++){
+        for (int i = 0; i < 16; i++) {
             nameBox.add(new Label(insrumentNames[i]));
         }
 
@@ -53,7 +97,6 @@ public class DrumPad {
         background.add(BorderLayout.WEST, nameBox);
 
         theFrame.getContentPane().add(background);
-
         GridLayout grid = new GridLayout(16,16);
         grid.setVgap(1);
         grid.setHgap(2);
@@ -66,8 +109,6 @@ public class DrumPad {
             checkboxList.add(c);
             mainPanel.add(c);
         }
-
-        setUpMidi();
 
         theFrame.setBounds(50, 50, 300, 300);
         theFrame.pack();
@@ -83,24 +124,29 @@ public class DrumPad {
             sequencer.setTempoInBPM(120);
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("setUpMidi");
         }
     }
 
     public void buildTrackAndStart() {
-        int[] trackList = null;
 
+        ArrayList<Integer> trackList = null;
         sequence.deleteTrack(track);
         track = sequence.createTrack();
 
         for (int i = 0; i < 16; i++) {
-            trackList = new int[16];
-            int key = instruments[i];
+
+            trackList = new ArrayList<Integer>();
+
+            //
+
             for (int j = 0; j < 16; j++) {
                JCheckBox js = (JCheckBox) checkboxList.get(j + (16*i));
                if (js.isSelected()) {
-                   trackList[j] = key;
+                   int key = instruments[i];
+                   trackList.add(new Integer(key));
                } else {
-                   trackList[j] = 0;
+                   trackList.add(null);
                }
             }
             makeTracks(trackList);
@@ -115,6 +161,7 @@ public class DrumPad {
             sequencer.setTempoInBPM(120);
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("164");
         }
     }
 
@@ -144,12 +191,90 @@ public class DrumPad {
         }
     }
 
-    public void makeTracks(int[] list) {
+    public class MySendListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            boolean[] checkboxState = new boolean[256];
+            for(int i = 0; i < 256; i++){
+               JCheckBox check = (JCheckBox) checkboxList.get(i);
+               if(check.isSelected()) {
+                   checkboxState[i] = true;
+               }
+            }
+            String messageToSend = null;
+            JEditorPane userMessage = null;
+            try {
+                out.writeObject(userName + nextNum++ + ": " + userMessage.getText());
+                out.writeObject(checkboxState);
+            } catch (Exception ex) {
+                System.out.println("Sorry dude. Could not sent it to server.");
+            }
+            userMessage.setText("");
+        }
+    }
+
+    public class MyListSelectionListener implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent le) {
+            if (!le.getValueIsAdjusting()) {
+                String selected = (String) incomingList.getSelectedValue();
+                if (selected != null) {
+                    boolean[] selectedState = (boolean[]) otherSeqsMap.get(selected);
+                    changeSequence(selectedState);
+                    sequencer.stop();
+                    buildTrackAndStart();
+                }
+            }
+        }
+    }
+
+    public class RemoteReader implements Runnable {
+        boolean[] checkboxState = null;
+        String nameToShow = null;
+        Object obj = null;
+        public void run() {
+            try {
+                while ((obj = in.readObject()) != null) {
+                    System.out.println("got an object from server");
+                    System.out.println(obj.getClass());
+                    String nameToShow = (String) obj;
+                    checkboxState = (boolean[]) in.readObject();
+                    otherSeqsMap.put(nameToShow, checkboxState);
+                    lineVector.add(nameToShow);
+                    incomingList.setListData(lineVector);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("run");
+            }
+        }
+    }
+
+    public class MyPlayMineListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (mySequence != null) {
+                sequence = mySequence;
+            }
+        }
+    }
+
+    public void changeSequence(boolean[] checkboxState) {
+        for (int i = 0; i < 256; i++) {
+            JCheckBox check = (JCheckBox) checkboxList.get(i);
+            if (checkboxState[i]) {
+                check.setSelected(true);
+            } else {
+                check.setSelected(false);
+            }
+        }
+    }
+
+    public void makeTracks(ArrayList list) {
+        Iterator it = list.iterator();
         for (int i = 0; i < 16; i++) {
-            int key = list[i];
-            if (key != 0) {
-                track.add((makeEvent(144,9, key,100, i)));
-                track.add((makeEvent(128,9, key,100, i+1)));
+            Integer num =(Integer) it.next();
+            if (num != 0) {
+                int numkey = num.intValue();
+                track.add((makeEvent(144,9, numkey,100, i)));
+                track.add((makeEvent(128,9, numkey,100, i+1)));
             }
         }
     }
@@ -160,7 +285,10 @@ public class DrumPad {
             ShortMessage a = new ShortMessage();
             a.setMessage(comd, chan, one, two);
             event = new MidiEvent(a, tick);
-        } catch (Exception e) { }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("makeEvent");
+        }
         return event;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 class Main {
     public static void main(String[] args) {
-        new DrumPad().buildGUI();
+        if (args.length != 0) {
+            new DrumPad().startUp(args[0]);
+        } else {
+            System.out.println("args[o] error");
+        }
     }
 }
 
